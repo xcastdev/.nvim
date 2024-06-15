2d205605b1f83ae2746c12217490d9bcb8ba440e
xcastdev <xcastdev0619@gmail.com>
2024-06-14 22:07:30 -0500

    added nextjs and tailwind lsp configurations

--- init.lua
+++ init.lua
@@ -849,7 +849,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'javascript', 'typescript', 'tsx', 'css' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
